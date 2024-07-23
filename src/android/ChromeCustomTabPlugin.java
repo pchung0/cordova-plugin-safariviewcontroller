8cf8dfef5e70739a4929bb20b1940c9c4ce19813
Ping Chun Chung <bingo61016@gmail.com>
2024-07-23 14:58:51 +0800

    Ensures await hide() functions correctly

--- src/android/ChromeCustomTabPlugin.java
+++ src/android/ChromeCustomTabPlugin.java
@@ -141,6 +141,7 @@ public class ChromeCustomTabPlugin extends CordovaPlugin{
             }
             case "hide": {
                 cordova.getActivity().startActivity(new Intent(cordova.getActivity(), cordova.getActivity().getClass()));
+                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, true));
                 return true;
             }
         }
